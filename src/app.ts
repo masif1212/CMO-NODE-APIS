942c0d72d1c322070876900decc3be11c71c3b3b
Haider Ali <143487236+HaiderAli170@users.noreply.github.com>
2025-04-24 17:56:33 +0500

    my changes

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,11 @@ import express from "express";
 import usersRouter from "./modules/users/router";
 import { errorHandler } from "./middleware/errorHandler";
 import pageSpeedRouter from "./modules/pagespeed/router";
-import session from "express-session";
-import passport from "passport";
-import dotenv from "dotenv";
-import "./config/passport";
-import authRouter from "./modules/google_auth/router";
-import googleRouter from "./modules/google_auth/router";
-dotenv.config();
-
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 // Mount the router on the correct path
 app.use("/api/users", usersRouter);
 
@@ -21,23 +14,5 @@ app.use("/api/users", usersRouter);
 app.use(errorHandler);
 
 app.use("/api/pagespeed", pageSpeedRouter);
-
-//GOOGLE AUTHENTICATION
-
-// 🔐 Session setup (required for storing tokens)
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET!,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      secure: false, // set to true if using HTTPS
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24, // 1 day
-    },
-  })
-);
-app.use("/api/auth", authRouter);
-app.use("/ga", googleRouter);
 
 export default app;
